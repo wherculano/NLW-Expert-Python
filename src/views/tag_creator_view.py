ea37410ca74f24198e27b53d1c0b4c12e0442f50
Wagner Herculano <wagherculano@hotmail.com>
2024-07-18 22:20:02 -0300

    Added class responsible for interacting with HTTP requests

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,18 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
+
+class TagCreatorView:
+    """ 
+    Responsible for interacting with HTTP
+    """
+
+    def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+        body = http_request.body
+        product_code = body["product_code"]
+
+        # business rules
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)
