edee94329bda684c1f7752ada2de5d294c494808
Wagner Herculano <wagherculano@hotmail.com>
2024-07-19 21:44:39 -0300

    Created error handler

---
+++ src/errors/error_handler.py
@@ -0,0 +1,27 @@
+from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
+
+def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body = {
+                "errors": [
+                    {
+                        "title": error.name,
+                        "detail": "error.studio."
+                    }
+                ]
+            }
+        )
+    return HttpResponse(
+        status_code=500,
+        body = {
+            "errors": [
+                {
+                    "title": "Server Error",
+                    "details": str(error)
+                }
+            ]
+        }
+    )
